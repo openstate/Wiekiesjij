b37d1a6aaec8532e040e8b7e5d203ff22e68128c
Harro van der Klauw <hvdklauw@MacKlauw>
2009-11-23 16:37:18 +0100

    Removed duplicate middleware in debug mode
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20091123153718-usoeuxs7rw5lnlpg

--- settings.py
+++ settings.py
@@ -116,7 +116,6 @@ if DEBUG:
     #debug_toolbar
     MIDDLEWARE_CLASSES += (
         'debug_toolbar.middleware.DebugToolbarMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
     )
     
     INTERNAL_IPS = ('127.0.0.1',)
