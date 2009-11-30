397a05cba4f9ce7fc77c7e6e71b2a2a48a86d5cc
Bernard McMahon <bernard@bernard-desktop>
2009-11-30 13:44:55 +0100

    fixed form list return
    --HG--
    extra : convert_revision : bernard%40bernard-desktop-20091130124455-nkrhbln8y5oce5n1

--- political_profiles/__init__.py
+++ political_profiles/__init__.py
@@ -1,12 +1,12 @@
 from political_profiles.models import PoliticianProfile, ChanceryProfile, ContactProfile, VisitorProfile
-#from political_profiles.forms import PoliticianProfileForm, ChanceryProfileForm, ContactProfileForm
+from political_profiles.forms import PoliticianProfileForm, ChanceryProfileForm, ContactProfileForm
 
 
 model_map = {
-    'candidate': 'PoliticanProfile',
-    'visitor': 'VisitorProfile',
-    'council_admin': 'ChanceryProfile',
-    'party_admin':  'ContactProfile',
+    'candidate': PoliticianProfile,
+    'visitor': VisitorProfile,
+    'council_admin': ChanceryProfile,
+    'party_admin':  ContactProfile,
 }
 
 def get_profile_model(for_function):
@@ -20,26 +20,26 @@ def get_profile_forms(for_function, type):
         Get a list of forms to use for the <type> action
     """
     politician_form_map = {
-            'create': ['PoliticianProfileForm'],
-            'edit': ['PoliticianProfileForm'],
-            'invite': ['PoliticianProfileForm'],
+            'create': [PoliticianProfileForm],
+            'edit': [PoliticianProfileForm],
+            'invite': [PoliticianProfileForm],
             }
     chancery_form_map = {
-            'create': ['ChanceryProfileForm'],
-            'edit': ['ChanceryProfileForm'],
-            'invite': ['ChanceryProfileForm'],
+            'create': [ChanceryProfileForm],
+            'edit': [ChanceryProfileForm],
+            'invite': [ChanceryProfileForm],
             }
     contact_form_map = {
-            'create': ['ContactProfileForm'],
-            'edit': ['ContactProfileForm'],
-            'invite': ['ContactProfileForm'],
+            'create': [ContactProfileForm],
+            'edit': [ContactProfileForm],
+            'invite': [ContactProfileForm],
             }
 
-    if model_map[for_function] == 'PoliticanProfile':
+    if model_map[for_function] == PoliticianProfile:
         return politician_form_map[type]
-    elif model_map[for_function] == 'ChanceryProfile':
+    elif model_map[for_function] == ChanceryProfile:
         return chancery_form_map[type]
-    elif model_map[for_function] == 'ContactProfile':
+    elif model_map[for_function] == ContactProfile:
         return contact_form_map[type]
-
+    
     return []
