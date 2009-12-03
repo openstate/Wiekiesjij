268e71886767d26f6424b3283c57184761ef3b02
J A Harkema <jaharkema@ubuntu>
2009-12-03 09:46:26 +0100

    Generic form loading
    --HG--
    extra : convert_revision : jaharkema%40ubuntu-20091203084626-nlgaf2ixhzr4ookz

--- political_profiles/views.py
+++ political_profiles/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from utils.multipathform import MultiPathFormWizard, Step
+
+# Even though your IDE might say this is an unused import, please do not remove the following Form imports
+from political_profiles.forms import *
+from elections.forms import *
+
+def instance_view(request, profile_type):
+    try:
+        formslist = dict(
+            generic_form = globals()[profile_type],
+        )
+    except KeyError:
+        raise NameError(profile_type + ' is not an existing form')
+
+
+    step_args = dict(
+        forms = formslist,
+    )
+
+    steps = Step('add_election_instance', forms=step_args['forms'], template='backoffice/wizard/step1.html')
+    politician_profile_wizard = MultiPathFormWizard(steps)
+
+    return politician_profile_wizard(request)
--- urls.py
+++ urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
+    (r'^(?P<profile_type>[-\w]+)/', 'political_profiles.views.instance_view'),
     (r'^backoffice', include('backoffice.urls')),
-
 )
 
 
