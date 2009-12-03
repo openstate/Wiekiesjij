733605985852ca6ff639331323707b5977985f0c
Harro van der Klauw <hvdklauw@MacKlauw>
2009-12-03 09:49:04 +0100

    Added documentation and docstrings for the utils
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20091203084904-bx0ymeujaniiswmj

--- utils/templatetags/email_tags.py
+++ utils/templatetags/email_tags.py
@@ -2,35 +2,20 @@ from django import template
 
 register = template.Library()
 
-
-class InsertStyleGroupNode(template.Node):
-    
-    def __init__(self, stylegroup):
-        self.stylegroup = template.Variable(stylegroup)
-        
-    def render(self, context):
-        stylegroup = self.stylegroup.resolve(context)
-        try:
-            email_classes = context['email_classes']
-            return email_classes[stylegroup]
-        except KeyError:
-            pass
-        return ''
-        
-        
-    @classmethod
-    def tag(cls, parser, token):
-        try:
-            tag, stylegroup = token.split_contents()
-        except ValueError:
-            raise template.TemplateSyntaxError, "%r tag requires exactly one arguments" % token.contents.split()[0]
-        return cls(stylegroup)
-        
-register.tag('use_stylegroup', InsertStyleGroupNode.tag)
-
-
 class StyleGroupNode(template.Node):
-    
+    """
+        Create a stylegroup.
+        
+        Stylegroups can be used to define css for inline styling in email and group them under a label.
+        
+        This way you have to define a style one and use it multiple times
+        
+        Example:
+            {% stylegroup 'header' %}
+                font-familty: verdana, helvetica;
+                font-size: 12px;
+            {% end_stylegroup %}
+    """
     def __init__(self, name, nodelist):
         self.name = template.Variable(name)
         self.nodelist = nodelist
@@ -60,3 +45,38 @@ class StyleGroupNode(template.Node):
         return cls(stylegroup, nodelist)
         
 register.tag('stylegroup', StyleGroupNode.tag)
+
+
+
+class InsertStyleGroupNode(template.Node):
+    """
+        Node for using a stylegroup
+        
+        Example:
+            <h1 style="{% use_stylegroup 'header %}">Example</h1>
+            
+        Will insert the captured bit of css from the `StyleGroupNode`
+        
+    """
+    def __init__(self, stylegroup):
+        self.stylegroup = template.Variable(stylegroup)
+        
+    def render(self, context):
+        stylegroup = self.stylegroup.resolve(context)
+        try:
+            email_classes = context['email_classes']
+            return email_classes[stylegroup]
+        except KeyError:
+            pass
+        return ''
+        
+        
+    @classmethod
+    def tag(cls, parser, token):
+        try:
+            tag, stylegroup = token.split_contents()
+        except ValueError:
+            raise template.TemplateSyntaxError, "%r tag requires exactly one arguments" % token.contents.split()[0]
+        return cls(stylegroup)
+        
+register.tag('use_stylegroup', InsertStyleGroupNode.tag)
