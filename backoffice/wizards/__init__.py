4e187711bbc47a7a74c609994c151e8534d2c7df
Harro van der Klauw <hvdklauw@MacKlauw>
2009-12-08 20:36:52 +0100

    Refactored wizards
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20091208193652-p8cfu8ouvmarjvml

---
+++ backoffice/wizards/__init__.py
@@ -0,0 +1,7 @@
+
+#FIXME: Propper imports
+from backoffice.wizards.electioninstance import *
+from backoffice.wizards.council import *
+from backoffice.wizards.party import *
+from backoffice.wizard.first_login import *
+from backoffice.wizard.candidate import *
--- backoffice/wizards/electioninstance.py
+++ backoffice/wizards/electioninstance.py
@@ -1,29 +1,23 @@
 import datetime
 
 from django.db import transaction
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, redirect, get_object_or_404
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.core.urlresolvers import resolve
-from elections.settings import ELECTION_INSTANCE_PARTY_LIST_LENGTH_INITIAL
+from django.shortcuts import redirect, get_object_or_404
+from django.core.urlresolvers import reverse
 
 from utils.multipathform import Step, MultiPathFormWizard
 
 from elections import settings
-#from elections.forms import InitialElectionInstanceForm,InitialCouncilForm, ElectionInstanceForm, CouncilForm, CouncilContactInformationForm, CandidacyForm, CouncilStylingSetupForm, ElectionInstanceSelectPartiesForm
-from elections.forms import *
 from elections.functions import get_profile_forms, create_profile, profile_invite_email_templates
-
-from elections.models import ElectionInstance, Council, ElectionEvent, Candidacy, ElectionInstanceParty
-
-
-from political_profiles.models import ChanceryProfile, PoliticianProfile
-from political_profiles.forms import PoliticianProfileForm, ChanceryProfileForm, ChanceryContactInformationForm, LinkForm, PoliticalExperienceForm, EducationForm, AppearenceForm, InterestForm, WorkExperienceForm
-
-
+from elections.forms import InitialElectionInstanceForm, EditElectionInstanceForm
+from elections.forms import ElectionInstanceForm, ElectionInstanceSelectPartiesForm
+from elections.forms import CouncilForm, CouncilStylingSetupForm, CouncilContactInformationForm
+from elections.models import ElectionInstance, Council, ElectionEvent
 from invitations.models import Invitation
+
+#TODO: remove these, use the get_profile_forms function instead
+from political_profiles.models import ChanceryProfile
+from political_profiles.forms import ChanceryProfileForm, ChanceryContactInformationForm
 
 class AddElectionInstanceWizard(MultiPathFormWizard):
     """
@@ -99,7 +93,7 @@ class AddElectionInstanceWizard(MultiPathFormWizard):
             
             #Create the invitation
             templates = profile_invite_email_templates('council_admin')
-            invitation = Invitation.create(
+            Invitation.create(
                 user_from=request.user, 
                 user_to=profile.user,
                 view='',
@@ -109,7 +103,7 @@ class AddElectionInstanceWizard(MultiPathFormWizard):
                 plain_template=templates['plain'],
                 )
             
-        except Exception, e:
+        except Exception:
             transaction.rollback()
             raise
         else:
@@ -163,7 +157,7 @@ class EditElectionInstanceWizard(MultiPathFormWizard):
             ei.modules = self.ei_data['modules']
             
             
-        except Exception, e:
+        except Exception:
             transaction.rollback()
             raise
         
@@ -173,7 +167,9 @@ class EditElectionInstanceWizard(MultiPathFormWizard):
         if request.POST.get('next', 'overview') == 'overview':
             return redirect('backoffice.election_event')
         raise NotImplementedError('Implement a redirect to the council edit wizard here.')
-                    
+        
+        
+#TODO: use profile_form function for the forms !
 class ElectionSetupWizard(MultiPathFormWizard):
     """
     Steps 2.1.5 - 2.6 of interaction design.
@@ -198,7 +194,7 @@ class ElectionSetupWizard(MultiPathFormWizard):
             # Checking if user really exists and if election_instanc exists. Getting those and passing it to the wizard.
             self.election_instance = ElectionInstance.objects.get(id=self.election_instance_id)
             self.user = User.objects.get(id=self.user_id)
-            self.chancery_profile = ChanceryProfile.objects.get(user=self.user_id)
+            self.chancery_profile = ChanceryProfile.objects.get(user=self.user_id) #TODO: User.profile should return a chancery profile !
         except Exception, e:
             raise e
 
@@ -316,296 +312,3 @@ class ElectionSetupWizard(MultiPathFormWizard):
         if request.POST.get('next', 'overview') == 'overview':
             return redirect(reverse('backoffice.election_setup_done'))
         raise NotImplementedError('Implement a redirect to the council edit wizard here.')
-
-
-
-@transaction.commit_manually
-class PoliticianProfileWizard(MultiPathFormWizard):
-    """
-        Wizard for a candidate to editing their own profile
-    """
-    def __init__(self, *args, **kwargs):
-        # Getting "user_id"
-        """
-        try:
-            self.user_id = kwargs['user_id'],
-
-            # Checking if user really exists and if election_instanc exists. Getting those and passing it to the wizard.
-
-            self.user = User.objects.get(id=self.user_id)
-            #self.chancery_profile = ChanceryProfile.objects.get(user=self.user_id)
-        except Exception, e:
-            raise e
-        """
-
-        step1_forms = dict(initial_candidate = PoliticianProfileForm,)
-        step2_forms = dict(link=LinkForm,)
-        step3_forms = dict(intrest=InterestForm,)
-        step4_forms = dict(appearence=AppearenceForm,)
-        step5_forms = dict(work= WorkExperienceForm,)
-        step6_forms = dict(education=EducationForm,)
-        step7_forms = dict(political=PoliticalExperienceForm,)
-
-        step1 = Step('candidate_edit',
-                    forms=step1_forms,
-                    template='backoffice/wizard/politician_profile/step1.html',)
-        step2 = Step('link',
-                    forms=step2_forms,
-                    template='backoffice/wizard/politician_profile/step2.html',)
-        step3 = Step('interest',
-                    forms=step3_forms,
-                     template='backoffice/wizard/politician_profile/step3.html',)
-        step4 = Step('appearance',
-                    forms=step4_forms,
-                    template='backoffice/wizard/politician_profile/step4.html',)
-        step5 = Step('work',
-                    forms=step5_forms,
-                    template='backoffice/wizard/politician_profile/step5.html',)
-        step6 = Step('education',
-                    forms=step6_forms,
-                    template='backoffice/wizard/politician_profile/step6.html',)
-        step7 = Step('political',
-                    forms=step7_forms,
-                    template='backoffice/wizard/politician_profile/step7.html',)
-                    
-        scenario_tree = step1.next(step2.next(step3.next(step4.next(step5.next(step6.next(step7))))))
-
-        #default template is the base, each step can override it as needed (for buttons)
-
-        template = 'backoffice/wizard/politician_profile/base.html',
-
-        super(PoliticianProfileWizard, self).__init__(scenario_tree, template)
-
-
-    def get_next_step(self, request, next_steps, current_path, forms_path):
-        return 0
-
-    def done(self, request, form_dict):
-        try:
-            # This needs to be easier !?!
-            for path, forms in form_dict.iteritems():
-                for name, form in forms.iteritems():
-                    if name == 'initial_candidate':
-                        #create election instance
-                        self.candidate_data = form.cleaned_data
-                    elif name == 'link':
-                        self.link = form.cleaned_data
-                    elif name == 'intrest':
-                        self.intrest = form.cleaned_data
-                    elif name == 'appearence':
-                        self.appearence = form.cleaned_data
-                    elif name == 'work':
-                        self.work = form.cleaned_data
-                    elif name == 'political':
-                        self.political = form.cleaned_data
-                    elif name == 'education':
-                        self.education = form.cleaned_data
-
-
-            
-            #Get the polictician profile
-            #politician = PoliticalProfile.objects.get(user_id=self.user.id)
-            #print politician
-
-
-        except Exception, e:
-            transaction.rollback()
-            raise e
-        else:
-            transaction.commit()
-
-        if request.POST.get('next', 'overview') == 'overview':
-            return redirect('backoffice.profile_complete')
-        raise NotImplementedError('Implement a redirect to the council edit wizard here.')
-
-
-
-class AddElectionPartyWizard(MultiPathFormWizard):
-    def __init__(self, instance, position, *args, **kwargs):
-        self.election_instance = instance
-        step1_forms = dict(
-            initial_ep=InitialElectionPartyForm,
-        )
-        step1_forms['initial_ep'].set_num_lists(instance.num_lists)
-        idx = 0;
-        for profile_form in get_profile_forms('party_admin', 'invite'):
-            step1_forms.update({'invite_contact_%s' % idx : profile_form})
-            idx += 1
-        step1 = Step('electionparty', 
-            forms=step1_forms,
-            template='backoffice/wizard/addelectionparty/step1.html',
-            initial={'initial_ep': {'position': position}}
-        )
-        #default template is the base, each step can override it as needed (for buttons)
-        template = 'backoffice/wizard/addelectionparty/base.html',
-        super(AddElectionPartyWizard, self).__init__(step1, template, *args, **kwargs)
-        
-    def get_next_step(self, request, next_steps, current_path, forms_path):
-        return 0
-        
-    def done(self, request, form_dict):
-        # This needs to be easier !?!
-        for path, forms in form_dict.iteritems():
-            for name, form in forms.iteritems():
-                if name == 'initial_ep':
-                    #create election instance 
-                    self.ep_data = form.cleaned_data
-                else:
-                    if not hasattr(self, 'invite_data'):
-                        self.invite_data = {}
-                    self.invite_data.update(form.cleaned_data)
-
-        party = Party.objects.create(
-            region = self.election_instance.council.region,
-            level = self.election_instance.council.level,
-            name = self.ep_data['name'],
-            abbreviation = self.ep_data['abbreviation'])
-        
-        eip = ElectionInstanceParty.objects.create(
-            party=party,
-            election_instance=self.election_instance,
-            position=self.ep_data['position'],
-            list_length=10)
-        
-        #Invite party admin
-        
-        return HttpResponseRedirect("%sthankyou/" % (request.path))
-
-class ElectionPartySetupWizard(MultiPathFormWizard):
-    def __init__(self, eip, *args, **kwargs):
-        self.eip = eip
-
-        step1_forms = dict(
-            election_party_contact_form=ElectionPartyContactForm
-        )
-        step2_forms = dict(
-            election_party_additional_form=ElectionPartyAdditionalForm
-        )
-        step3_forms = dict(
-            election_party_description_form=ElectionPartyDescriptionForm
-        )
-
-        initial = eip.party.__dict__
-        initial.update({'list_length': eip.list_length})
-
-        step1 = Step('election_party_contact_form',
-                    forms=step1_forms,
-                    initial={'election_party_contact_form': initial},
-                    template='backoffice/wizard/setupelectionparty/step1.html',)
-        step2 = Step('election_party_additional_form',
-                    forms=step2_forms,
-                    initial={'election_party_additional_form': initial},
-                    template='backoffice/wizard/setupelectionparty/step2.html',)
-        step3 = Step('election_party_description_form',
-                    forms=step3_forms,
-                    initial={'election_party_description_form': initial},
-                    template='backoffice/wizard/setupelectionparty/step3.html',)
-        template = 'backoffice/wizard/election_party_setup/base.html',
-        super(ElectionPartySetupWizard, self).__init__(step1.next(step2.next(step3)), template, *args, **kwargs)
-
-    def get_next_step(self, request, next_steps, current_path, forms_path):
-        return 0
-
-    def done(self, request, form_dict):
-        data = {}
-        for path, forms in form_dict.iteritems():
-            for name, form in forms.iteritems():
-                data.update(form.cleaned_data)
-        self.eip.party.name = data['name']
-        self.eip.party.abbreviation = data['abbreviation']
-        self.eip.party.address_street = data['address']['street']
-        self.eip.party.address_number = data['address']['number']
-        self.eip.party.address_postalcode = data['address']['postalcode']
-        self.eip.party.address_city = data['address']['city']
-        self.eip.party.email = data['email']
-        self.eip.party.website = data['website']
-        self.eip.party.slogan = data['slogan']
-        #self.eip.party.logo = data['logo']
-        #num_seats??
-        self.eip.party.description = data['description']
-        self.eip.party.history = data['history']
-        self.eip.party.manifesto = data['manifesto']
-        self.eip.party.save()
-
-        self.eip.list_length = int(data['list_length'])
-        self.eip.save()
-
-        return HttpResponseRedirect(reverse('backoffice.election_instance_view', args=[self.eip.election_instance.id]))
-
-
-
-class AddCandidateWizard(MultiPathFormWizard):
-    """
-        Wizard for adding a Candidate
-    """
-
-    def __init__(self, *args, **kwargs):
-        self.election_instance_party_id = 1 #kwargs['election_instance_party_id']
-        self.position = 1 #kwargs['position']
-        step1_forms = dict()
-        idx = 0;
-        for profile_form in get_profile_forms('candidate', 'invite'):
-            step1_forms.update({'invite_contact_%s' % idx : profile_form})
-            idx += 1
-        step1 = Step('candidate',
-            forms = step1_forms,
-            template = 'backoffice/wizard/addcandidate/step1.html',
-        )
-        template = 'backoffice/wizard/addcandidate/base.html'
-        super(AddCandidateWizard, self).__init__(step1, template)
-
-    def get_next_step(self, request, next_steps, current_path, forms_path):
-        return 0
-
-    @transaction.commit_manually
-    def done(self, request, form_dict):
-
-        try:
-            for path, forms in form_dict.iteritems():
-                for name, form in forms.iteritems():
-                    if name == 'initial_form':
-                        self.form_data = form.cleaned_data
-                    else:
-                        if not hasattr(self, 'form_data'):
-                            self.form_data = {}
-                        self.form_data.update(form.cleaned_data)
-
-            #Store data
-            tmp_data = {
-                'first_name': self.form_data['name']['first_name'],
-                'middle_name': self.form_data['name']['middle_name'],
-                'last_name': self.form_data['name']['first_name'],
-                'email': self.form_data['email'],
-                'gender': self.form_data['gender'],
-            }
-            self.candidate = create_profile('candidate', tmp_data)
-
-            #Link candidate to party
-            candidacy = Candidacy(
-                election_party_instance = get_object_or_404(ElectionInstanceParty, party=self.election_instance_party_id),
-                candidate = self.candidate.user,
-                position = self.position,
-            )
-            candidacy.save()
-
-            #Create invitation
-            templates = profile_invite_email_templates('candidate')
-            invitation = Invitation.create(
-                user_from = request.user,
-                user_to = self.candidate.user,
-                view = '',
-                text = 'Invitation text',
-                subject = 'Invitation',
-                html_template = templates['html'],
-                plain_template = templates['plain'],
-            )
-
-        except Exception, e:
-            transaction.rollback()
-            raise
-        else:
-            transaction.commit()
-        #TODO: Make args=[1] dynamic. In addcandidate/step1.html too ('Cancel'-link)
-
-
-        return HttpResponseRedirect(reverse('backoffice.election_party_view', args=[1]))
