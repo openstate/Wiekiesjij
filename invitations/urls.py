db729d2fb90bb3e8773dcc3129cbd3d7a2870380
ws52 <polichism@polichism-desktop>
2009-12-10 11:18:19 +0100

    - create a 'notexist' view - redirects if the users are using an invalid hash
    --HG--
    extra : convert_revision : polichism%40polichism-desktop-20091210101819-z6wyip4uqvga6cl2

--- invitations/urls.py
+++ invitations/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url('^(?P<hash>[a-zA-Z0-9]{32})/$', 'invitations.views.index', name='invitations.index'),
     url('^(?P<hash>[a-zA-Z0-9]{32})/accept/$', 'invitations.views.accept', name='invitations.accept'),
     url('^(?P<hash>[a-zA-Z0-9]{32})/existing/$', 'invitations.views.existing', name='invitations.existing'),
+    url('notexist/$', 'invitations.views.notexist', name='invitations.notexist'),
 )
