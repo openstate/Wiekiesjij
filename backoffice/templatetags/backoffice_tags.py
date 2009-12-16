e5a00995cd3026f35090c5df3426c44e4f9eb0d9
Harro van der Klauw <hvdklauw@MacKlauw>
2009-12-16 20:32:18 +0100

    Fixed template tags
    --HG--
    rename : backoffice/template_tags/__init__.py => backoffice/templatetags/__init__.py
    rename : backoffice/template_tags/backoffice_tags.py => backoffice/templatetags/backoffice_tags.py
    extra : convert_revision : hvdklauw%40macklauw-20091216193218-7xl64ubh37a8skju

--- backoffice/templatetags/backoffice_tags.py
+++ backoffice/templatetags/backoffice_tags.py
@@ -2,8 +2,6 @@ from django import template
 
 register = template.Library()
 
-
-
 class IfProfileNode(template.Node):
     """
         If Node to check if the user has the correct profiletype
@@ -18,19 +16,19 @@ class IfProfileNode(template.Node):
     """
     PROFILE_TYPE = ['council_admin', 'party_admin', 'candidate']
     
-    def __init__(self, type, nodelist_true, nodelist_false):
-        self.type = template.Variable(type)
+    def __init__(self, profile_type, nodelist_true, nodelist_false):
+        self.profile_type = template.Variable(profile_type)
         self.nodelist_true = nodelist_true
         self.nodelist_false = nodelist_false
         
     def render(self, context):
-        type = self.type.resolve(context)
+        profile_type = self.profile_type.resolve(context)
         user = context['user']
         
-        type_index = self.PROFILE_TYPE.index(type)
+        type_index = self.PROFILE_TYPE.index(profile_type)
         if not user.is_authenticated():
             return self.nodelist_false.render(context)
-        elif user.is_staff():
+        elif user.is_staff:
             return self.nodelist_true.render(context)
         elif user.profile is not None and self.PROFILE_TYPE.index(user.profile.type) <= type_index:
             return self.nodelist_true.render(context)
@@ -44,7 +42,6 @@ class IfProfileNode(template.Node):
         if len(tokens) < 2:
             raise template.TemplateSyntaxError('{0} tag requires one profile.type as arguments'.format(tokens[0]))
     
-        
         nodelist_true = parser.parse(('else', 'endifprofile'))    
         token = parser.next_token()
         if token.contents == 'else':
