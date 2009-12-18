389e09c7d95653fa54f2d7c9250916219efcc715
Harro van der Klauw <hvdklauw@MacKlauw>
2009-12-18 10:12:20 +0100

    Fixed context processor not giving a clear error
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20091218091220-3grlzenbqtde3ohh

--- elections/context_processors.py
+++ elections/context_processors.py
@@ -2,7 +2,4 @@ from elections import settings
 from elections.models import ElectionEvent
 
 def election_event(request):
-    try:
-        return {'election_event': ElectionEvent.objects.get(pk=settings.ELECTION_EVENT_ID)}
-    except:
-        pass
+    return {'election_event': ElectionEvent.objects.get(pk=settings.ELECTION_EVENT_ID)}
