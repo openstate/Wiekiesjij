52c33406bd5ca5dfc2b7d6d7cd657c7eff8b96de
Harro van der Klauw <hvdklauw@MacKlauw>
2009-12-24 16:31:36 +0100

    Invitation limit
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20091224153136-qreawfkpvjgk4yat

--- invitations/management/commands/send_invitations.py
+++ invitations/management/commands/send_invitations.py
@@ -10,8 +10,8 @@ class Command(LabelCommand):
     label = 'profile_type'
     
     def handle_label(self, profile_type, *args, **options):
-        #get at max 50 rows from the database
-        invitations = Invitation.objects.filter(type=profile_type, accepted=False, send_on=None).order_by('created')[:50]
+        #get at max 5 rows from the database
+        invitations = Invitation.objects.filter(type=profile_type, accepted=False, send_on=None).order_by('created')[:4]
         invitations_temp = list(invitations)
         #stores ids in a variable from invitations
         invitation_ids = [x.pk for x in invitations_temp]
