4d5528b3c63ce2ea21931e4520dcb7357a6b5eb7
Harro van der Klauw <hvdklauw@MacKlauw>
2010-01-25 10:20:28 +0100

    Cleanup
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20100125092028-z9a7ejcllxw2ldzj

--- frontoffice/views.py
+++ frontoffice/views.py
@@ -1,7 +1,8 @@
 
 import re
 import hashlib
-from django.http import Http404
+import datetime
+
 from django.conf import settings
 from django.core.cache import cache
 from django.utils.translation import ugettext_lazy as _
@@ -9,20 +10,16 @@ from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template.context import RequestContext
 from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
+from django.db.models import Q
+from django.core.paginator import Paginator
 
 from frontoffice.forms import PoliticianFilterForm, VisitorProfileForm, RegionSelectForm
 from elections.models import Candidacy, ElectionInstance, ElectionInstanceParty
 from political_profiles.models import PoliticianProfile, PoliticalGoal, GoalRanking, VisitorProfile
-from utils.functions import list_unique_order_preserving
-from django.db.models import Q
-import datetime
-
 from political_profiles.models import RELIGION, DIET, MARITAL_STATUS, GENDERS
-
 from frontoffice.decorators import visitors_only
 
-from django.core.paginator import Paginator
-from questions.forms.types import ModelAnswerForm
+
 from frontoffice.wizards.match import BestCandidate
 
 def redirect_view(request):
@@ -35,22 +32,14 @@ def redirect_view(request):
     else:
         return redirect('bo.redirect')
 
-def new_url(path, field, value):
+#Helper function
+def _new_url(path, field, value):
     old_str = field + '='  + str(value)
     old_str = old_str.replace(' ','+')
 
     new_str = field + '='
 
     return path.replace(old_str, new_str)
-
-def answer_question(request, election_instance_party_id, user_id=None):
-    '''
-        AnswerQuestion - wizard.
-        @param int election_instance_party_id - ElectionInstanceParty id
-        @param int user_id User (Candidate=PoliticalProfile) id
-    '''
-    check_permissions(request, election_instance_party_id, 'candidate')
-    return AnswerQuestion(election_instance_party_id=election_instance_party_id, user_id=user_id)(request)
 
 def test(request, election_instance_id = None, iframe=None):
     if not election_instance_id:
@@ -122,7 +111,7 @@ def politician_profile_filter(request):
                 politicians = PoliticianProfile.objects.filter(pk__in=candidates).order_by('?')
                 filtered_politicians = politicians
  
-                new_path = new_url(path, 'region', form.cleaned_data['region'].id)
+                new_path = _new_url(path, 'region', form.cleaned_data['region'].id)
                 region_filtered = True
 
                 if 'ElectionInstance' in request.session:
@@ -137,22 +126,22 @@ def politician_profile_filter(request):
                     else:
                         name_filter = name_filter &  (Q(last_name__icontains=name) | Q(first_name__icontains=name) | Q(middle_name__icontains=name))
                 filtered_politicians = filtered_politicians.filter(name_filter)
-                new_path = new_url(path, 'name', form.cleaned_data['name'])
+                new_path = _new_url(path, 'name', form.cleaned_data['name'])
                 filters.append((_('Name'), form.cleaned_data['name'], new_path))
 
             if form.cleaned_data['gender'] != 'All' and form.cleaned_data['gender']:
                 filtered_politicians = filtered_politicians.filter(gender=form.cleaned_data['gender'])
-                new_path = new_url(path, 'gender', form.cleaned_data['gender'])
+                new_path = _new_url(path, 'gender', form.cleaned_data['gender'])
                 filters.append((_('Gender'), gender[form.cleaned_data['gender']], new_path))
 
             if form.cleaned_data['children']  != '---------' and form.cleaned_data['children']:
                 if form.cleaned_data['children'] == 1:
                     filtered_politicians = filtered_politicians.filter(num_children__gte=1)
-                    new_path = new_url(path, 'children', form.cleaned_data['children'])
+                    new_path = _new_url(path, 'children', form.cleaned_data['children'])
                     filters.append((_('Children'), _('Yes'), new_path))
                 else:
                     filtered_politicians = filtered_politicians.filter(num_children=0)
-                    new_path = new_url(path, 'children', form.cleaned_data['children'])
+                    new_path = _new_url(path, 'children', form.cleaned_data['children'])
                     filters.append((_('Children'), _('No'), new_path))
 
 
@@ -168,7 +157,7 @@ def politician_profile_filter(request):
                 date = datetime.date(todate.year - (form.cleaned_data['start_age']), todate.month, todate.day)
 
                 filtered_politicians = filtered_politicians.filter(dateofbirth__lt=date)
-                new_path = new_url(path, 'start_age', form.cleaned_data['start_age'])
+                new_path = _new_url(path, 'start_age', form.cleaned_data['start_age'])
                 filters.append((_('Youngest'), form.cleaned_data['start_age'], new_path))
 
             if form.cleaned_data['end_age'] is not None:
@@ -176,48 +165,48 @@ def politician_profile_filter(request):
 
                 filtered_politicians = filtered_politicians.filter(dateofbirth__gte=date)
 
-                new_path = new_url(path, 'end_age', form.cleaned_data['end_age'])
+                new_path = _new_url(path, 'end_age', form.cleaned_data['end_age'])
                 filters.append((_('Oldest'), form.cleaned_data['end_age'], new_path))
 
             if form.cleaned_data['education'] != '---------' and form.cleaned_data['education']:
                 filtered_politicians = filtered_politicians.filter(education__level=form.cleaned_data['education'])
-                new_path = new_url(path, 'education', form.cleaned_data['education'].id)
+                new_path = _new_url(path, 'education', form.cleaned_data['education'].id)
 
                 filters.append((_('Education'), form.cleaned_data['education'], new_path))
 
             if form.cleaned_data['political_exp_years']:
                 filtered_politicians = filtered_politicians.filter(political_experience_days__gte=(form.cleaned_data['political_exp_years'] * 365))
-                new_path = new_url(path, 'political_exp_years', form.cleaned_data['political_exp_years'])
+                new_path = _new_url(path, 'political_exp_years', form.cleaned_data['political_exp_years'])
                 filters.append((_('Years political experience'), form.cleaned_data['political_exp_years'], new_path))
 
             if form.cleaned_data['work_exp_years']:
                 filtered_politicians = filtered_politicians.filter(work_experience_days__gte=(form.cleaned_data['work_exp_years'] * 365))
-                new_path = new_url(path, 'work_exp_years', form.cleaned_data['work_exp_years'])
+                new_path = _new_url(path, 'work_exp_years', form.cleaned_data['work_exp_years'])
                 filters.append((_('Years work experience'), form.cleaned_data['work_exp_years'], new_path))
 
             if form.cleaned_data['religion'] != '---------' and form.cleaned_data['religion']:
                 filtered_politicians = filtered_politicians.filter(religion=form.cleaned_data['religion'])
-                new_path = new_url(path, 'religion', form.cleaned_data['religion'])
+                new_path = _new_url(path, 'religion', form.cleaned_data['religion'])
                 filters.append((_('Religion'), religion[form.cleaned_data['religion']], new_path))
 
             if form.cleaned_data['marital_status'] != '---------' and form.cleaned_data['marital_status']:
                 filtered_politicians = filtered_politicians.filter(marital_status=form.cleaned_data['marital_status'])
-                new_path = new_url(path, 'marital_status', form.cleaned_data['marital_status'])
+                new_path = _new_url(path, 'marital_status', form.cleaned_data['marital_status'])
                 filters.append((_('Marital status'), marital_status[form.cleaned_data['marital_status']], new_path))
 
             if form.cleaned_data['goals'] != '---------' and form.cleaned_data['goals']:
                 filtered_politicians = filtered_politicians.filter(goals__goal__icontains=form.cleaned_data['goals'])
-                new_path = new_url(path, 'goals', form.cleaned_data['goals'])
+                new_path = _new_url(path, 'goals', form.cleaned_data['goals'])
                 filters.append((_('Goals'), form.cleaned_data['goals'], new_path))
 
             if form.cleaned_data['smoker'] != '---------' and form.cleaned_data['smoker']:
                 if form.cleaned_data['smoker'] == '1':
                     filtered_politicians = filtered_politicians.filter(smoker=1)
-                    new_path = new_url(path, 'smoker', form.cleaned_data['smoker'])
+                    new_path = _new_url(path, 'smoker', form.cleaned_data['smoker'])
                     filters.append((_('Smoker'), _('Yes'), new_path))
                 else:
                     filtered_politicians = filtered_politicians.filter(smoker=0)
-                    new_path = new_url(path, 'smoker', form.cleaned_data['smoker'])
+                    new_path = _new_url(path, 'smoker', form.cleaned_data['smoker'])
                     filters.append((_('Smoker'), _('No'), new_path))
 
             if form.cleaned_data['diet'] != '---------' and form.cleaned_data['diet']:
@@ -231,7 +220,7 @@ def politician_profile_filter(request):
                     filtered_politicians = filtered_politicians.filter(diet='vegan')
                 if form.cleaned_data['diet'] == '':
                     filtered_politicians = filtered_politicians.filter(diet='other')
-                new_path = new_url(path, 'diet', form.cleaned_data['diet'])
+                new_path = _new_url(path, 'diet', form.cleaned_data['diet'])
                 filters.append((_('Vegitarian'), diet[form.cleaned_data['diet']], new_path))
             filtered_politicians.distinct()
             
