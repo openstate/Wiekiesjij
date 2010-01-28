7f0782ad8b5d7c87cd5ac94f14b239101ae087f6
J A Harkema <jaharkema@ubuntu>
2010-01-28 16:02:07 +0100

    Added mailing to comment flagging
    --HG--
    extra : convert_revision : jaharkema%40ubuntu-20100128150207-4pla05ne0363juf8

--- authorized_comments/models.py
+++ authorized_comments/models.py
@@ -1,11 +1,35 @@
 from django.contrib.comments.models import Comment
+from django.contrib.comments import signals
+from django.core.mail import mail_managers
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext as _
+from django.contrib.sites.models import Site
+
+def comment_flagged(sender, **kwargs):
+    """
+        Signal handler
+    """
+    flag = kwargs['flag']
+
+    if flag.flag == flag.SUGGEST_REMOVAL:
+
+        current_site = Site.objects.get_current()
+        domain = current_site.domain
+
+        subject = _('A comment was flagged')
+        message = _("A comment was flagged as inappropiate. Check out http://%s" % domain+reverse('fo.goal', args=[flag.comment.object_pk]) ) #Hardcoded goal. Maybe change?
+        mail_managers(subject, message, fail_silently=False)
+        
+
+    
+
+# Connect the signal handler to the signal invoker
+signals.comment_was_flagged.connect(comment_flagged)
 
 class AuthorizedComment(Comment):
-    #TODO: Fix this today: 
 
     def get_flag_url(self):
         return "/auth_comments/flag/%s/" % self.id
 
     def get_delete_url(self):
         return "/auth_comments/delete/%s/" % self.id
-
