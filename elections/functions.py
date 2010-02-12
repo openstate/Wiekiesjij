826defbb25e28947b447297c7962e4a8cf0c11da
Harro van der Klauw <hvdklauw@MacKlauw>
2010-02-12 10:53:56 +0100

    Fixed pop
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20100212095356-6mj3yjt4x07x1tsg

--- elections/functions.py
+++ elections/functions.py
@@ -88,7 +88,7 @@ def get_popularity(election_instance_id):
             FROM elections_candidacy ec 
             JOIN elections_electioninstanceparty p ON p.id = ec.election_party_instance_id
             LEFT JOIN frontoffice_candidateanswers ca ON ca.candidate_id = ec.candidate_id
-            LEFT JOIN political_profiles_userstatistics us ON ec.candidate_id = ca.candidate_id
+            LEFT JOIN political_profiles_userstatistics us ON ec.candidate_id = us.user_id
             WHERE p.election_instance_id = %s
             GROUP BY ec.id
         """
