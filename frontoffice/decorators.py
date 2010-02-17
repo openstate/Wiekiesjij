d4501eb8ac061b60962d000671be2a629ff25f40
Harro van der Klauw <hvdklauw@MacKlauw>
2010-02-17 08:39:33 +0100

    Small fix
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20100217073933-rmogt05vv73klew0

--- frontoffice/decorators.py
+++ frontoffice/decorators.py
@@ -10,7 +10,7 @@ def visitors_only(function=None, redirect_field_name=REDIRECT_FIELD_NAME):
         to the log-in page if necessary.
     """
     actual_decorator = user_passes_test(
-        lambda u: (u.profile is not None and 'visitor' == u.profile.type),
+        lambda u: (u.is_authenticated() and u.profile is not None and 'visitor' == u.profile.type),
         login_url=settings.PERMISSION_DENIED_URL,
         redirect_field_name=redirect_field_name
     )
@@ -25,7 +25,7 @@ def politicians_only(function=None, redirect_field_name=REDIRECT_FIELD_NAME):
         to the log-in page if necessary.
     """
     actual_decorator = user_passes_test(
-        lambda u: (u.profile is not None and 'candidate' == u.profile.type),
+        lambda u: (u.is_authenticated() and u.profile is not None and 'candidate' == u.profile.type),
         login_url=settings.PERMISSION_DENIED_URL,
         redirect_field_name=redirect_field_name
     )
