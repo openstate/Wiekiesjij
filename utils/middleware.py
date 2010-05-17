db1a112c8f81f4beb88c3a4164a16be94ad9e312
Harro van der Klauw <hvdklauw@MacKlauw>
2010-05-17 11:06:24 +0200

    Added http auth middleware
    --HG--
    extra : convert_revision : hvdklauw%40macklauw-20100517090624-lmfd4ofvty4pnpcd

--- utils/middleware.py
+++ utils/middleware.py
@@ -1,6 +1,8 @@
 import json
 import re
 import sys
+import base64
+from functools import wraps
 
 from django.views.debug import technical_500_response
 from django.conf import settings as django_settings
@@ -10,6 +12,68 @@ from utils import settings
 from utils.models import PostLog
 from django.http import HttpResponseRedirect
 
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+
+class HttpAuthMiddleware(object):
+    """
+    Some middleware to authenticate all requests at this site.
+    """
+    def process_request(self, request):
+        return _http_auth_helper(request)
+
+def http_auth(func):
+    """
+    A decorator, that can be used to authenticate some requests at the site.
+    """
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        result = _http_auth_helper(request)
+        if result is not None:
+            return result
+        return func(request, *args, **kwargs)
+    return inner
+
+
+def _http_auth_helper(request):
+    "This is the part that does all of the work"
+    try:
+        if not django_settings.FORCE_HTTP_AUTH:
+            # If we don't mind if django's session auth is used, see if the
+            # user is already logged in, and use that user.
+            if request.user:
+                return None
+    except AttributeError:
+        pass
+        
+    # At this point, the user is either not logged in, or must log in using
+    # http auth.  If they have a header that indicates a login attempt, then
+    # use this to try to login.
+    if request.META.has_key('HTTP_AUTHORIZATION'):
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == 'basic':
+                # Currently, only basic http auth is used.
+                uname, passwd = base64.b64decode(auth[1]).split(':')
+                user = authenticate(username=uname, password=passwd)
+                if user:
+                    # If the user successfully logged in, then add/overwrite
+                    # the user object of this request.
+                    request.user = user
+                    return None
+    
+    # The username/password combo was incorrect, or not provided.
+    # Challenge the user for a username/password.
+    resp = HttpResponse()
+    resp.status_code = 401
+    try:
+        # If we have a realm in our settings, use this for the challenge.
+        realm = django_settings.HTTP_AUTH_REALM
+    except AttributeError:
+        realm = ""
+    
+    resp['WWW-Authenticate'] = 'Basic realm="%s"' % realm
+    return resp
 
 
 
