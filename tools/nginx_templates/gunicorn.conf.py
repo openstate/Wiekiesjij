d3fe47d1f35a85d04017a9504b01321851af62a0
Jeffrey Gelens <jeffrey@gelens.org>
2011-03-03 15:06:53 +0100

    Updated workers from 9 to 2

--- tools/nginx_templates/gunicorn.conf.py
+++ tools/nginx_templates/gunicorn.conf.py
@@ -2,6 +2,6 @@ user = '%(user_name)s'
 group = '%(user_name)s'
 bind = 'unix:///var/run/%(hostname)s.sock'
 pidfile = '/var/run/%(hostname)s.pid'
-workers = '9'
+workers = '2'
 deamon = True
 worker_class = 'gevent'
