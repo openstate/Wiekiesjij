8c91278562e5847f90c32771a423cc60846f4fbd
Jaap Bouma <jaap.bouma@gmail.com>
2012-08-21 20:36:37 +0200

    Some more occurences of Provinciale Staten replace with Tweede Kamer

--- source/elections/management/commands/send_result_sms.py
+++ source/elections/management/commands/send_result_sms.py
@@ -16,20 +16,20 @@ class Command(BaseCommand):
                 help='Shows the number of text messages that will be send for the election instance'),
         )
     help = 'Sends sms for a certain election instnace'
-    
+
     def handle(self, *args, **options):
-        
+
         list_eis = options.get('list')
         send = options.get('send')
         fake = options.get('fake')
-        
+
         accepte_credit = get_credit()
-        
+
         if list_eis:
             for ei in ElectionInstance.objects.filter(modules__slug='SMS').filter(visitor_results__sent__isnull=True).distinct().all():
                 print "%s - %s" % (ei.pk, ei.name)
             return True
-        
+
         if fake or send:
             id = fake or send
             try:
@@ -37,19 +37,19 @@ class Command(BaseCommand):
             except:
                 print "Election Instance not found"
                 return False
-            
+
             if ei.modules.filter(slug='SMS').count() == 0 or ei.visitor_results.filter(sent__isnull=True).count() == 0:
                 print "Nothing to do"
                 return False
-                
+
             if accepte_credit > 0:
-                
+
                 recipients = ei.visitor_results.filter(sent__isnull=True).distinct('telephone').filter(telephone__isnull=False).values_list('telephone', flat=True)
                 recipients = list(set(recipients))
-                
+
                 if send:
                     for r in recipients:
-                        sendsms('Wiekiesjij', [r], 'Vandaag tot 21:00: Provinciale Staten verkiezingen! U kunt stemmen in ieder stemlokaal (binnen uw eigen gemeente). Vergeet uw stempas en ID-bewijs niet!', datetime.now())
+                        sendsms('Wiekiesjij', [r], 'Vandaag: Tweede Kamerverkiezingen! U kunt stemmen in ieder stemlokaal (binnen uw eigen gemeente). Vergeet uw stempas en ID-bewijs niet!', datetime.now())
                     ei.visitor_results.update(sent=datetime.now())
                     accepte_credit = accepte_credit - len(recipients)
                 elif fake:
