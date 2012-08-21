773555216354e127e1ad68ce9dee0e0204b67bd9
Jaap Bouma <jaap.bouma@gmail.com>
2012-08-21 15:52:37 +0200

    Best guess modification of election event

--- source/elections/management/commands/add_election_event.py
+++ source/elections/management/commands/add_election_event.py
@@ -4,20 +4,20 @@ from django.core.management import BaseCommand
 
 from elections.models import ElectionEvent
 
- 
+
 class Command(BaseCommand):
     help = 'Adds an electionevent'
-    
+
     def handle(self, *args, **options):
         ee = ElectionEvent.objects.create(
-            name='Provinciale Statenverkiezingen 2011',
+            name='Tweede Kamerverkiezingen 2012',
             parent_region='Nederland',
             level='landelijk',
-            default_date=datetime.datetime(2011, 3, 2),
+            default_date=datetime.datetime(2012, 9, 12),
             question_due_period=7,
             profile_due_period=7,
-            candidate_due_period=30,
-            party_due_period=33,
+            candidate_due_period=14,
+            party_due_period=16,
         )
         print "ElectionEvent added with id %d" % ee.pk
-        
+
