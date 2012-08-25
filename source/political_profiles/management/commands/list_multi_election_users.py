f1cd8c57b16345594559b0f0b72636f6fa4d7544
Jaap Bouma <jaap.bouma@gmail.com>
2012-08-25 19:57:22 +0200

    Added manage command for listing users that participated in previous elections as well as this one

---
+++ source/political_profiles/management/commands/list_multi_election_users.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+from django.contrib.auth.models import User
+from django.db.models import Count
+from political_profiles.models import ConnectionType, WorkExperienceSector, PoliticalExperienceType, EducationLevel
+
+class Command(BaseCommand):
+    help = 'Lists those users who are candidates in multiple different elections'
+
+    def handle(self, *args, **options):
+	users = User.objects.annotate(num_elect = Count('elections')).filter(num_elect__gt = 1)
+	print "%s users participating in multiple elections" % users.count()
+	for u in users:
+	    for c in u.elections.select_related('election_party_instance__party'):
+		print u.email, u.profile.last_name, c.election_party_instance.party
+
+# candidacy = self.user.elections.select_related('election_party_instance__party').order_by('position')
+#         if candidacy.count() != 0:
+#		return candidacy[0].election_party_instance.party
