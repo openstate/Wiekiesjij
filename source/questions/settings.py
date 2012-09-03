5131d2302264bd3e5b4fd345e1a77c70315e12fe
Jaap Bouma <jaap.bouma@gmail.com>
2012-09-04 00:18:16 +0200

    Fixed double in question weights

--- source/questions/settings.py
+++ source/questions/settings.py
@@ -13,7 +13,7 @@ PROFILE_QUESTION_WEIGHT_OPTIONS = (
     ('q10', 'Religieuze motivatie kandidaat'),
     ('q11', 'Leeftijd kandidaat'),
     ('q12', 'Geslacht kandidaat'),
-    ('q12', 'Coalitie'),
+    ('q13', 'Coalitie'),
 )
 
 
