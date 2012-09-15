5adbd9f7c5b9ddec08974e005ab1c45be987c546
Jaap Bouma <jaap.bouma@gmail.com>
2012-09-15 12:17:36 +0200

    Added mark_elected method to Admin interface for Candidacies Made candidacies more human readable in list view

--- source/elections/admin.py
+++ source/elections/admin.py
@@ -9,7 +9,7 @@ admin.site.register(Council, CouncilAdmin)
 
 class ElectionEventAdmin(admin.ModelAdmin):
     pass
-    
+
 admin.site.register(ElectionEvent, ElectionEventAdmin)
 
 class ElectionInstanceAdmin(admin.ModelAdmin):
@@ -37,9 +37,20 @@ class PartyAdmin(admin.ModelAdmin):
 
 admin.site.register(Party, PartyAdmin)
 
+
 class CandidacyAdmin(admin.ModelAdmin):
-    pass
-    
+    list_display = ('candidate_name', 'election_party_instance', 'position', 'elected')
+    list_filter = ('election_party_instance', )
+    actions = ['mark_elected',]
+
+
+    # Yes, this is inefficient. Fixit.
+    def candidate_name(self,obj):
+        return "%s %s" % (obj.candidate.profile.first_name, obj.candidate.profile.last_name)
+
+    def mark_elected(self, request, queryset):
+        queryset.update(elected=True)
+
 admin.site.register(Candidacy, CandidacyAdmin)
 
 class ElectionInstanceModuleAdmin(admin.ModelAdmin):
